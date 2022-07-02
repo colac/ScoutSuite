012aed3709451461fad72df8d5b18575b18cae9f
hugo <hugof_lacerda@hotmail.com>
2022-07-02 09:16:26 -0700

    workflow to create docker image with python 3.8

--- docker/bin/container-install-scoutsuite.sh
+++ docker/bin/container-install-scoutsuite.sh
@@ -10,9 +10,19 @@ TMPDIR=/tmp
 # =====================================
 # install ScoutSuite
 # =====================================
-cd ${WORKDIR}
-virtualenv -p python3 scoutsuite
-source ${WORKDIR}/scoutsuite/bin/activate
-pip install scoutsuite
+# cd ${WORKDIR}
+# virtualenv -p python3.8 scoutsuite
+# source ${WORKDIR}/scoutsuite/bin/activate
+# pip3 install scoutsuite
+# echo -e "\n\nScoutsuite Installation Complete!\n\n"
 
+
+
+cd ${WORKDIR}
+git clone https://github.com/nccgroup/ScoutSuite.git --branch 5.12.0-rc1
+cd ScoutSuite
+#virtualenv -p python3 scoutsuite3.8
+#source ${WORKDIR}/ScoutSuite/scoutsuite3.8/bin/activate
+pip install -r requirements.txt
+#python scout.py --help
 echo -e "\n\nScoutsuite Installation Complete!\n\n"
--- docker/bin/container-set-init.sh
+++ docker/bin/container-set-init.sh
@@ -2,6 +2,8 @@
 cat <<'EOF' >> /root/.bashrc
 export TERM=linux
 cd ${HOME}
-source ${HOME}/scoutsuite/bin/activate
+
 echo -e "Welcome to Sscoutsuite!\nYou are already in the Scoutsuite virtual environment, so just type \`scout\` to run it!\n    (for example: \`scout -h\` to see the help documentation).\n\nHave fun!\n\n"
 EOF
+
+#source ${HOME}/ScoutSuite/scoutsuite3.8/bin/activate
